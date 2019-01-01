370523029cbb57d4dedcf4ced0d5c563eddd189a
Haley Sulcer <hjsulcer@gmail.com>
2019-01-01 15:26:57 -0500

    implemented data into War.js and added <br />s in skill-cost, skill-cool, and skill-req sections

--- divinity-skills/src/components/Skills/War.js
+++ divinity-skills/src/components/Skills/War.js
@@ -1,13 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import './War.css';
+import './Skills.css';
 
-const War = () => {
-  return (
-      <div className="War-wrap">
-        <h1>This is the Warfare Page</h1>
-      </div>
-  );
+class War extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+    this.componentWillMount = this.componentWillMount.bind(this);
+  }
+
+  componentWillMount() {
+    fetch('https://api.myjson.com/bins/l0yys')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({ items: data });
+    });
+  }
+
+
+  render() {
+
+    const { items } = this.state;
+    
+    return (
+
+      <div className="war-wrap">
+
+        <span className="skill-title"><h1>Warfare</h1></span>
+
+      <div className="War">
+
+        
+        { items.map((item, num) => {
+
+          return (
+
+            <div className="skill-wrap">
+
+                <div className="skill-name">
+                  <p key={num}>{item.name}</p>
+                </div>
+
+                <tg>
+
+                  <div className="tier-2">
+                
+                    <th><div className="skill-cost">
+                      <p key={num}><strong>Memory:<br /></strong> {item.cost}</p>
+                    </div></th>
+
+                    <th><div className="skill-cool">
+                      <p key={num}><strong>Cooldown:<br /></strong> {item.cool} turns</p>
+                    </div></th>
+
+                    <th><div className="skill-req">
+                      <p key={num}><strong>Requirements:<br /></strong> {item.req}</p>
+                    </div></th>
+
+                  </div>
+              </tg>
+
+              <tg>
+                <th><div className="skill-desc">
+                  <p key={num}>{item.desc}</p>
+                </div></th>
+              </tg>
+
+              <tg>
+                <th><div className="skill-img">
+                  <p>[Image]</p>
+                </div></th>
+              </tg>
+                
+            </div>
+          );
+        })}
+        </div>
+
+        </div>
+    );
+  }
 }
 
 export default War;
+
+
+
