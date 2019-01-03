8cc5a2b14dc3efa89724a855f9626dad62afef99
Haley Sulcer <hjsulcer@gmail.com>
2019-01-03 16:00:36 -0500

    implemented skills into Necro.js and styled Necro page

--- divinity-skills/src/components/Skills/Necro.js
+++ divinity-skills/src/components/Skills/Necro.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import './Necro.css';
+import './Skills.css';
 
-const Necro = () => {
-  return (
-      <div className="Necro-wrap">
-        <h1>This is the Necromancer Page</h1>
-      </div>
-  );
+class Necro extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+    this.componentWillMount = this.componentWillMount.bind(this);
+  }
+
+  componentWillMount() {
+    fetch('https://api.myjson.com/bins/r1a04')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({ items: data });
+    });
+  }
+
+
+  render() {
+
+    const { items } = this.state;
+    
+    return (
+
+      <div className="necro-wrap">
+
+        <span className="skill-title"><h1>Necromancer</h1></span>
+
+        <div className="Necro">
+
+          
+          { items.map((item, num) => {
+
+            return (
+
+              <div className="skill-wrap">
+
+                  <div className="skill-name">
+                    <p key={num}>{item.name}</p>
+                  </div>
+
+                  <tg>
+
+                    <div className="tier-2">
+                  
+                    <th><div className="skill-cost">
+                      <p key={num}><strong>Memory:<br /></strong> {item.cost}</p>
+                    </div></th>
+
+                    <th><div className="skill-cool">
+                      <p key={num}><strong>Cooldown:<br /></strong> {item.cool} turns</p>
+                    </div></th>
+
+                    <th><div className="skill-req">
+                      <p key={num}><strong>Requirements:<br /></strong> {item.req}</p>
+                    </div></th>
+
+                    </div>
+                </tg>
+
+                <tg>
+                  <th><div className="skill-desc">
+                    <p key={num}>{item.desc}</p>
+                  </div></th>
+                </tg>
+
+                <tg>
+                  <th><div className="skill-img">
+                    <p>[Image]</p>
+                  </div></th>
+                </tg>
+                  
+              </div>
+            );
+          })}
+          </div>
+
+        </div>
+    );
+  }
 }
 
 export default Necro;
