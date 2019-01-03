8bbe8a555c6c25c137d9a1d5981048db2ff6ecb5
Haley Sulcer <hjsulcer@gmail.com>
2019-01-03 16:27:53 -0500

    imported all Skill categories (at least, the ones I have now) to Home.js

--- divinity-skills/src/Components/Home.js
+++ divinity-skills/src/Components/Home.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import Aero from './Skills/Aero.js';
+import Geo from './Skills/Geo.js';
+import Hunts from './Skills/Hunts.js';
+import Hydro from './Skills/Hydro.js';
+import Necro from './Skills/Necro.js';
+import Poly from './Skills/Poly.js';
+import Pyro from './Skills/Pyro.js';
+import Scoun from './Skills/Scoun.js';
+import Summ from './Skills/Summ.js';
+import War from './Skills/War.js';
+
 // import './Home.css';
 
 const Home = () => {
   return (
 
       <div className="home">
-        <h1>this is the Home Page</h1>
+        <h1>Browse all Skills</h1>
+
+        <Aero />
+        <Geo />
+        <Hunts />
+        <Hydro />
+        <Necro />
+        <Poly />
+        <Pyro />
+        <Scoun />
+        <Summ />
+        <War />
+
       </div>
 
       
