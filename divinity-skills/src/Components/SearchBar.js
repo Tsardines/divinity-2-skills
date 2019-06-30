d19ad0c5f20a84256858166db4eb47d90fbe769f
Haley Sulcer <hjsulcer@gmail.com>
2019-06-30 16:31:39 -0400

    worked on two-col layout

--- divinity-skills/src/Components/SearchBar.js
+++ divinity-skills/src/Components/SearchBar.js
@@ -4,24 +4,23 @@ import './SearchBar.css';
 import SearchResults from 'react-filter-search';
 
 class SearchBar extends Component {
-
     constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            value: ''
-        };
+      super(props);
+      this.state = {
+          data: [],
+          value: ''
+      };
     }
 
     componentWillMount() {
-        fetch('https://api.myjson.com/bins/pksng')
+      fetch('https://api.myjson.com/bins/pksng')
         .then(res => res.json())
         .then(json => this.setState({ data: json }));
     }
 
     handleChange = event => {
-        const { value } = event.target;
-        this.setState({ value });
+      const { value } = event.target;
+      this.setState({ value });
     };
 
     render() {
@@ -29,30 +28,30 @@ class SearchBar extends Component {
 
         return (
             <div className="skills-wrapper">
-                <input type="text" placeholder="skills, categories, keywords" value={value} onChange={this.handleChange} />
+              <input type="text" placeholder="skills, categories, keywords" value={value} onChange={this.handleChange} />
 
                 <SearchResults
                     value={value}
                     data={data}
                     renderResults={results => (
-                        <div class="all-skills-wrap">
-                            {results.map(el => (
-                                <div className="skill-section">
-                                    <table>
-                                        <tr>
-                                            <span className="td-name"><td><i>{el.name}</i></td></span>
-                                                <br />
-                                            <span className="td-desc"><td>{el.desc}</td></span>
-                                                <br />  
-                                            <span className="td-cat"><td>{el.cat}</td></span>
-                                            <span><td><strong>Memory:</strong> {el.cost} AP</td></span>
-                                            <span><td><strong>Cooldown:</strong> {el.cool}</td></span>
-                                            <span><td><strong>Requires:</strong> {el.req}</td></span>
-                                        </tr>
-                                    </table>
-                                </div>
-                            ))}
-                        </div>
+                      <div class="all-skills-wrap">
+                        {results.map(el => (
+                          <div className="skill-section">
+                              <table>
+                                <tr>
+                                  <span className="td-name"><td><i>{el.name}</i></td></span>
+                                      <br />
+                                  <span className="td-desc"><td>{el.desc}</td></span>
+                                      <br />  
+                                  <span className="td-cat"><td>{el.cat}</td></span>
+                                  <span><td><strong>Memory:</strong> {el.cost} AP</td></span>
+                                  <span><td><strong>Cooldown:</strong> {el.cool}</td></span>
+                                  <span><td><strong>Requires:</strong> {el.req}</td></span>
+                                </tr>
+                              </table>
+                          </div>
+                        ))}
+                      </div>
                     )}
                 />
 
